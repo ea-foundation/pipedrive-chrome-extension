af9f4ee5898e5c962cbbde29c433383f2bd6c502
Kim Korte <psaniko@gmail.com>
2017-11-10 18:23:33 +0100

    Removed activity filter, changed navigation order, fixed navigation in settings

--- content.js
+++ content.js
@@ -21,11 +21,6 @@ $(document).ready(function() {
               personViewAdjustments();
           }
 
-          if (!foundDealChange && isElementVisible('dealChange')) {
-            foundDealChange = true;
-            activityFeedAdjustments();
-          }
-
           if (!foundNavigation && document.getElementById('mainmenu')) {
               foundNavigation = true;
               navigationAdjustments();
@@ -51,7 +46,8 @@ $(document).ready(function() {
         }
         var totalAmount = $('.dealsOverview .legend .won>td:nth-child(4)').text();
         $('.dealsOverview .dealsTitle, .openDeals, .closedDealsChart').remove();
-        $('.dealsOverview .columnItem').text('Donations (' + totalDonations + '): ' + totalAmount);
+        $('.dealsOverview .columnItem').text('Deals (donations): ' + totalAmount);
+        $('.dealsOverview .viewDeals span').text('See all deals (' + totalDonations + ')');
 
         // Change "Add deal" to "Expand notes"
         $('.buttonAddDeal span.primary').removeClass('primary').text('Expand notes');
@@ -61,21 +57,17 @@ $(document).ready(function() {
             if (notesCollapsed) {
                 $('span', this).text('Collapse notes');
                 $('.showMore').click();
+                $('.pinnedNotes .expand').click();
+                $('.expandMessage button').click();
             } else {
                 $('span', this).text('Expand notes');
                 $('.showLess').click();
+                $('.pinnedNotes .collapse').click();
+                $('.collapseMessage button').click();
             }
             notesCollapsed = !notesCollapsed;
             event.stopPropagation();
         });
-    }
-
-    /**
-     * Modify elements in the activity feed in the person detail view.
-     */
-    function activityFeedAdjustments() {
-        // Hide "Deal created" events
-        $('.dealChange:has(.status.open)').hide();
     }
 
     /**
@@ -84,27 +76,26 @@ $(document).ready(function() {
     function navigationAdjustments() {
         $('.logo_container a').attr('href', '/persons'); // change logo target to person list
         $('.key-pipeline .label').text('Donations'); // change Deals to Donations
-        $('.key-pipeline').before(
+        $('.key-activities').after(
             '<li class="key-persons"><a href="/persons"><span class="icon icon-person" data-tooltip="Persons"></span><span class="label">Persons</span></a>',
             '<li class="key-organizations"><a href="/organizations"><span class="icon icon-organization" data-tooltip="Organizations"></span><span class="label">Organizations</span></a>'
         ); // add top-level items "Persons" and "Organizations"
         $('.key-contacts').remove(); // Remove dropdown Contacts item
-        $('.key-mailbox').remove(); // Remove Mail item
     }
 
     // listen for URL changes
     window.addEventListener('popstate', function () {
+        navigationAdjustments();
         personViewAdjustments();
     }, false);
 
     // initialize status variables
     var foundDealsBox = false;
-    var foundDealChange = false;
     var foundNavigation = false;
     var url = document.location.href;
 
     // configure and create observer to listen for DOM changes
-    target = document.getElementById('application');
+    target = document.body;
     var observer = new MutationObserver(mutationHandler);
     var config = { attributes: true, childList: true, subtree: true, characterData: true };
     observer.observe(target, config);
